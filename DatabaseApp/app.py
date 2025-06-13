8de6b10ebecf1eb66c547c69a1c1ffb9eaca24a4
shaxweb <igrayunagitare@gmail.com>
2025-06-14 01:54:28 +0500

    update

--- DatabaseApp/app.py
+++ DatabaseApp/app.py
@@ -1,62 +1,15 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template
 from flask_cors import CORS
 import json
 
 app = Flask(__name__)
+render = render_template
 CORS(app)
-have = 0
-users = []
 
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/")
 def home():
-    if request.method == "GET":
-        return jsonify({"status": True, "message": "HellosWorld!"})  # ✅ исправлено
-
-    elif request.method == "POST":
-        data = request.json
-        with open("datas.json", "w") as file:
-            json.dump(data, file, indent=4)  # ✅ записываем как JSON, а не просто text
-        return jsonify({"status": True, "message": "Saved!"})
-    else:
-        return jsonify({"message": "unknown"})
-
-
-@app.route("/api/users/", methods=["GET", "POST"])
-def users():
-    if request.method == "GET":
-        if have == 1:
-            with open("users.json", "r") as file:
-                data = file.read()
-                return jsonify({"sended": data})
-    
-        else:
-            with open("users.json", "w") as file:
-                json.dump([], file, indent=4)
-            return jsonify({"status": True})
-    elif request.method == "POST":
-        data = request.json
-        users = []
-        with open("users.json", "r") as file:
-            users = file.read()
-        users.append(data)
-        with open("users.json", "w") as file:
-            file.write(users, file, indent=4)
-        
-        return jsonify({"status": True})
-    
-    else:
-        return jsonify({"message": "unknown"})
-
-
-@app.route("/register/", methods=["GET", "POST"])
-def register():
-    if request.method == "POST":
-        data = request.json
-        return jsonify({"username": data["username"]})
-    
-    else:
-        return jsonify({"status": 200})
+    return render("index.html", name="Shaxrux")
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", debug=True)
