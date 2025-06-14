67129bab13c09426866c7f1be99577fdf82ff2a8
shaxweb <igrayunagitare@gmail.com>
2025-06-14 09:36:49 +0500

    update

--- DatabaseApp/sql.py
+++ DatabaseApp/sql.py
@@ -1,7 +1,9 @@
 import sqlite3 as sql
 
+db_file = "templates/data.db"
+
 # Создание таблицы — можно оставить глобально
-with sql.connect("data.db") as db:
+with sql.connect(db_file) as db:
     cur = db.cursor()
     cur.execute("""
     CREATE TABLE IF NOT EXISTS users(
@@ -13,7 +15,7 @@ with sql.connect("data.db") as db:
 
 # Всегда создавать новое подключение внутри функции
 def register(username, password):
-    with sql.connect("data.db") as db:
+    with sql.connect(db_file) as db:
         cur = db.cursor()
         cur.execute("INSERT INTO users(username, password) VALUES(?, ?)", (username, password))
         db.commit()
