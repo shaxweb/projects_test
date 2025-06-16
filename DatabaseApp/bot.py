a10078e75d96c948ba4c3987f0ef098fb1e07367
shaxweb <igrayunagitare@gmail.com>
2025-06-16 23:49:51 +0500

    update

--- DatabaseApp/bot.py
+++ DatabaseApp/bot.py
@@ -1,10 +1,16 @@
-from aiogram import Bot, Dispatcher, executor
-from aiogram.types import *
+import requests
 
-bot = Bot("8158445939:AAHmoesq6Em6F5QdxcNhRJYSVL2pTLpUyn0")
+BOT_TOKEN = "8158445939:AAHmoesq6Em6F5QdxcNhRJYSVL2pTLpUyn0"
+CHAT_ID = 6181120570
 
-async def send_message(message):
-  await bot.send_message(
-    chat_id=6181120570,
-    text=message
-  )
+def send_message(message):
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+    payload = {
+        "chat_id": CHAT_ID,
+        "text": message
+    }
+    response = requests.post(url, data=payload)
+    return response.json()
+
+# Пример использования
+send_message("Hello from requests!")
