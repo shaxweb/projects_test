ec417860157b82625724b6525dc81aaa9c0d9788
shaxweb <igrayunagitare@gmail.com>
2025-06-18 17:42:57 +0500

    update

--- MobileStore/app.py
+++ MobileStore/app.py
@@ -16,7 +16,21 @@ def home():
 
 @app.route("/register/", methods=["GET"])
 def register():
+    data = request.args
+    username, password = data.get("username"), data.get("password")
+    if not username:
+        return jsonify({"status": False, "message": "Username must not be empty"})
+    if not password:
+        return jsonify({"status": False, "message": "Password must not be empty"})
+    
+    for i in users:
+        if i["username"] == username:
+            return jsonify({"status": False, "message": "Username already taked"})
+    
+    users.append({"username": username, "password": password})
+    
   return jsonify({"status": True, "message": f"Datas: {request}"})
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", debug=True)
