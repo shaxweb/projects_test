6a068a60731e288b2c9d02db33cb2a3662f28f3b
shaxweb <igrayunagitare@gmail.com>
2025-06-18 11:20:49 +0500

    update

---
+++ MobileStore/apk/www/js/check_connection.js
@@ -0,0 +1,14 @@
+if (!navigator.onLine) {
+  window.location.assign("../main/connect_error.html");
+}
+
+window.addEventListener("offline", () => {
+  window.location.assign("../main/connect_error.html");
+});
+
+fetch("https://shaxweb-database.onrender.com/")
+.then(response => {})
+.catch(error => {
+  console.log(error.message)
+  window.location.assign("../main/connect_error.html")
+})
