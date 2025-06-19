f39e64ccbf8ef79f02c9a53d3f17c08cfc2879af
shaxweb <igrayunagitare@gmail.com>
2025-06-20 02:06:05 +0500

    update

--- MobileStore/app.py
+++ MobileStore/app.py
@@ -88,7 +88,7 @@ def auth_func():
             sql.delete_wait_user(user[0])
             return render("auth.html", status=True, username=user[1])
     
-    return render("auth.hrml", status=False, error="Token was broken")
+    return render("auth.html", status=False, error="Token was broken")
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", debug=True)
