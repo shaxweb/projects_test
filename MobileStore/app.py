fdb8bc2ae4c6fce5e59ab5287aa9f888cc6bbf63
shaxweb <igrayunagitare@gmail.com>
2025-06-19 12:17:02 +0500

    update

--- MobileStore/app.py
+++ MobileStore/app.py
@@ -6,27 +6,11 @@ app = Flask(__name__)
 render = render_template
 CORS(app)
 
-users = []
-wait_users = []
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/")
 def home():
-    return jsonify({"status": True, "message": "Hello, World!", "data": users})
-
-
-@app.route("/register/", methods=["GET", "POST"])
-def register():
-    if request.method == "POST":
-        data = request.json
-        username, password = data.get("username"), data.get("password")
-        
-        if username and password:
-            wait_users.append({"username": username, "password": password})
-            return jsonify({"status": True, "message": "email_sended"})
-        else:
-            return jsonify({"status": False, "message": "Uncorrect datas"})
-        
-    return jsonify({"status": True, "message": "User created"})
+    products = sql.get_all_data()["products"]
+    return jsonify({"status": True, "message": "Hello, Wolrd", "data": products})
 
 
 if __name__ == '__main__':
--- MobileStore/sql.py
+++ MobileStore/sql.py
@@ -25,9 +25,38 @@ CREATE TABLE IF NOT EXISTS products(
   id INTEGER PRIMARY KEY,
   title TEXT,
   description TEXT,
-  price TEXT
+  price INTEGER
 )
 """)
 
 db.commit()
+
+
+def get_all_data():
+  return {
+    "users": cur.execute("SELECT * FROM users").fetchall(),
+    "wait_users": cur.execute("SELECT * FROM wait_users").fetchall(),
+    "products": cur.execute("SELECT * FROM products").fetchall()
+  }
+
+
+def get_data(id):
+  return {
+    "user": cur.execute("SELECT * FROM users WHERE id = ?", (id,)).fetchone(),
+    "wait_user": cur.execute("SELECT * FROM wait_user WHERE id = ?", (id,)).fetchone(),
+    "product": cur.execute("SELECT * FROM product WHERE id = ?", (id,)).fetchone(),
+  }
+
+
+def create_product(title, description, price):
+  cur.execute("INSERT INTO products(title, description, price) VALUES(?,?,?)", (title, description, price))
+  db.commit()
+
+
+create_product("iPhone", "Phone", 1500)
+create_product("Samsung", "Best Phone", 1200)
+create_product("Nokia", "Phone", 1500)
+create_product("Novey", "Best Phone", 1200)
+create_product("Nivea", "Phone", 1500)
+create_product("BMW", "Best Phone", 1200)
 
