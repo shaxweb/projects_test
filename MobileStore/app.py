1c3dc376b0aa22b8a12bd1464311aaff37c5172e
shaxweb <igrayunagitare@gmail.com>
2025-06-21 21:07:19 +0500

    update app.py

--- MobileStore/app.py
+++ MobileStore/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify, request, render_template
 from flask_cors import CORS
-import scripts, json, sql
+import .scripts, json, .sql
 
 app = Flask(__name__)
 render = render_template
